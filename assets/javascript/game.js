c2cfce114c0d3e10672ab2ed1338b816f794a7db
DMa <davidweibinma@Weibins-MacBook-Pro.local>
2019-02-08 19:11:28 -0500

    added option 1 design notes implementation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,20 @@ var numLosses = 0;
 var totalScore = 0;
 
 $(document).ready(function () {
-    var randomNumGoal = Math.floor(Math.random() * 20) + 80;
+    var randomNumGoal = Math.floor(Math.random() * 80) + 19;
     $("#randomNumber").html(randomNumGoal);
-    var randomNum1 = Math.floor(Math.random() * 30);
-    var randomNum2 = Math.floor(Math.random() * 30);
-    var randomNum3 = Math.floor(Math.random() * 30);
-    var randomNum4 = Math.floor(Math.random() * 30);
+    var randomNum1 = Math.ceil(Math.random() * 12);
+    var randomNum2 = Math.ceil(Math.random() * 12);
+    var randomNum3 = Math.ceil(Math.random() * 12);
+    var randomNum4 = Math.ceil(Math.random() * 12);
 
     function reset() {
-        randomNumGoal = Math.floor(Math.random() * 20) + 80;
+        randomNumGoal = Math.floor(Math.random() * 80) + 19;
         $("#randomNumber").html(randomNumGoal);
-        randomNum1 = Math.floor(Math.random() * 30);
-        randomNum2 = Math.floor(Math.random() * 30);
-        randomNum3 = Math.floor(Math.random() * 30);
-        randomNum4 = Math.floor(Math.random() * 30);
+        randomNum1 = Math.ceil(Math.random() * 12);
+        randomNum2 = Math.ceil(Math.random() * 12);
+        randomNum3 = Math.ceil(Math.random() * 12);
+        randomNum4 = Math.ceil(Math.random() * 12);
         totalScore = 0;
         $("#currentScore").html(totalScore);
         console.log("we RESET");
